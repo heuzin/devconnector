4cd2b657529207e4548883c2b469811c1e8be5fa
Matheus Silva <msilvaqs@outlook.com>
2026-01-26 10:08:29 -0500

    Routes files with express router

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Define Routes
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
