0872662579245f051654f82910d8d1de7cfaf76d
Matheus Silva <msilvaqs@outlook.com>
2026-02-09 10:38:06 -0500

    Alert component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -16,6 +17,7 @@ const App = () => (
     <Router>
       <Fragment>
         <Navbar />
+        <Alert />
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/register" element={<Register />} />
