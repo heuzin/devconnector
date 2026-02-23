74c28a2c4c730e5aa6058f31402692bf5c6e04ff
Matheus Silva <msilvaqs@outlook.com>
2026-02-23 10:22:00 -0500

    Profile experience and education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,12 +5,13 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({ getProfileById, auth, profile: { profiles, loading } }) => {
   const { id } = useParams();
 
-  console.log(profiles);
   useEffect(() => {
     getProfileById(id);
   }, [getProfileById, id]);
@@ -35,6 +36,37 @@ const Profile = ({ getProfileById, auth, profile: { profiles, loading } }) => {
           <div class="profile-grid my-1">
             <ProfileTop profile={profiles} />
             <ProfileAbout profile={profiles} />
+            <div className="profile-exp bg-white p-2">
+              <h2 className="text-primary">Experience</h2>
+              {profiles.experience.length > 0 ? (
+                <Fragment>
+                  {profiles.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profiles.education.length > 0 ? (
+                <Fragment>
+                  {profiles.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
