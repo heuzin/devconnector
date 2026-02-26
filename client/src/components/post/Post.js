87785e311998fd0eb108809216769f9631252bf0
Matheus Silva <msilvaqs@outlook.com>
2026-02-26 10:34:01 -0500

    Adding comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading } }) => {
@@ -23,6 +24,7 @@ const Post = ({ getPost, post: { post, loading } }) => {
             Back To Posts
           </Link>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </Fragment>
       )}
     </div>
